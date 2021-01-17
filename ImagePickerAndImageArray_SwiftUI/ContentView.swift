4ba3095fc767dfe7f1f08b7bfa0bb01650a57b8e
Toxumuharu <ring.o.awlright@gmail.com>
2021-01-17 11:25:45 +0900

    Initial Commit

---
+++ ImagePickerAndImageArray_SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ImagePickerAndImageArray_SwiftUI
+//
+//  Created by Toxumuharu on 2021/01/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
