3f43f0e7b3b13e2166535102243a3e04c3867aea
Toxumuharu <ring.o.awlright@gmail.com>
2021-01-17 14:38:31 +0900

    add ImagePickerView with plus button on the top

--- ImagePickerAndImageArray_SwiftUI/ContentView.swift
+++ ImagePickerAndImageArray_SwiftUI/ContentView.swift
@@ -8,10 +8,68 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var isShowImagePicker = false
+    @State var image = UIImage()
+        
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+            ScrollView{
+                VStack{
+                    Image(uiImage: image)
+                        .resizable()
+                        .scaledToFill()
+                }.padding()
+                
+            }.navigationTitle("Add Image App")
+            .navigationBarItems(trailing: Button(action: {
+                isShowImagePicker.toggle()
+            }) {
+                Image(systemName: "plus")
+            })
+            .sheet(isPresented: $isShowImagePicker, content: {
+                ImagePickerView(isPresented: $isShowImagePicker, selectedImage: self.$image)
+            })
+        }
+        
     }
+}
+
+struct ImagePickerView: UIViewControllerRepresentable {
+    @Binding var isPresented: Bool
+    @Binding var selectedImage: UIImage
+    
+    func makeUIViewController(context: Context) -> some UIViewController {
+        let controller = UIImagePickerController() // write here Whiat kind of ViewController you want to return
+        controller.delegate = context.coordinator
+        return controller
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        return Coordinator(parent: self)
+    }
+    
+    // This is the tricky part
+    class Coordinator: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+        let parent: ImagePickerView
+        init(parent: ImagePickerView){
+            self.parent = parent
+        }
+        
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            if let selectedImageFromPicker = info[.originalImage] as? UIImage{
+                print(selectedImageFromPicker)
+                self.parent.selectedImage = selectedImageFromPicker
+            }
+            self.parent.isPresented = false
+        }
+    }
+    
+    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+    // empty is fine
+    }
+    
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
@@ -19,3 +77,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
